4227eb30489d507a71964ba3673a70993e6d6fd7
Lingsong Feng <lingsong.feng@outlook.com>
2025-02-18 00:25:26 +0800

    python and go lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,12 +2,14 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
         local lspconfig = require("lspconfig")
+
+        -- C/C++ Language Server
         lspconfig.clangd.setup({
             cmd = { "clangd" },
             filetypes = { "c", "cpp" },
             on_attach = function(_, bufnr)
                 local bufopts = { noremap = true, silent = true, buffer = bufnr }
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)  -- 跳转到定义
+                vim.keymap.set("n", "<Leader>gd", vim.lsp.buf.definition, bufopts)  -- 跳转到定义
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)        -- 查看文档
                 vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts) -- 查看引用
 
@@ -16,6 +18,21 @@ return {
                 end, { buffer = bufnr })
             end,
             capabilities = require("cmp_nvim_lsp").default_capabilities(), -- 让补全更强大
+        })
+
+        -- Go Language Server
+        lspconfig.gopls.setup({
+            on_attach = function(_, bufnr)
+                vim.keymap.set("n", "<Leader>fmt", function()
+                    vim.lsp.buf.format({ async = true })
+                end, { buffer = bufnr })
+            end,
+            capabilities = require("cmp_nvim_lsp").default_capabilities(), -- 让补全更强大
+        })
+
+        -- Python Language Server
+        lspconfig.pyright.setup({
+
         })
     end
 }
